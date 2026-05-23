9d18e6ac5977327b5379c65f82a02153e79c8dd5
vitorgramacho <vgfraga@live.com>
2026-05-23 15:02:48 -0300

    test: add insecure subprocess shell=True (B602)

--- services/targeting-service/app.py
+++ services/targeting-service/app.py
@@ -9,6 +9,7 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from functools import wraps
 import logging
+import subprocess
 
 # Configura o loggingg
 logging.basicConfig(level=logging.INFO)
@@ -18,6 +19,7 @@ log = logging.getLogger(__name__)
 load_dotenv() 
 
 app = Flask(__name__)
+subprocess.call('echo targeting-service iniciado', shell=True)
 
 # --- Configuração ---
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -203,3 +205,4 @@ if __name__ == '__main__':
     port = int(os.getenv("PORT", 8003))
     app.run(host='0.0.0.0', port=port, debug=False)
 # rebuild 2026-05-23 13:09:09
+
