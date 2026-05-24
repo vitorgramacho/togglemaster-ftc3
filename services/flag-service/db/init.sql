8b96d4d6aa02a91df77e60d0143e0d2fc76936a9
vitorgramacho <43939344+vitorgramacho@users.noreply.github.com>
2026-05-24 16:50:07 -0300

    Update init.sql

--- services/flag-service/db/init.sql
+++ services/flag-service/db/init.sql
@@ -1,16 +1,13 @@
 CREATE TABLE IF NOT EXISTS flags (
     id SERIAL PRIMARY KEY,
 
-    -- 'name' é a chave de negócio única (ex: 'enable-new-checkout')
     name VARCHAR(100) UNIQUE NOT NULL, 
     description TEXT,
-    -- Este é o 'kill switch' global. Se for false, a flag está desativada para todos.
     is_enabled BOOLEAN NOT NULL DEFAULT false,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- Opcional, mas boa prática: Trigger para atualizar 'updated_at' automaticamente
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -19,7 +16,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Remove o trigger se já existir, para evitar erro na recriação
 DROP TRIGGER IF EXISTS set_timestamp ON flags;
 
 CREATE TRIGGER set_timestamp
